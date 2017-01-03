9647f11b02ee79d7c510831b4297a2f8631f665a
ajay <ajaysunarthi@iitj.ac.in>
2017-01-03 23:44:16 +0530

    all options

--- pixelMe.js
+++ pixelMe.js
@@ -5,5 +5,16 @@ function pixelMe(options) {
     if (!options.targetElement) {
         throw new Error('targetElement is not passed in options');
     }
-	
+    // default options
+    var defaults = {
+        'image': null,
+        'tileWidth': 3,
+        'tileHeight': 3,
+        'targetElement': null,
+        'tileShape': 'circle',
+        'opacity': 1,
+        'width': null,
+        'height': null
+    };
+
 }
