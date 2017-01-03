26041d2a8ed8db7b88bea8ccf927dec89ab79d61
ajay <ajaysunarthi@iitj.ac.in>
2017-01-03 23:22:34 +0530

    options should be given

--- pixelMe.js
+++ pixelMe.js
@@ -1,3 +1,9 @@
-function pixelMe() {
+function pixelMe(options) {
+	if (!options.image) {
+        throw new Error('image is not passed in options');
+    }
+    if (!options.targetElement) {
+        throw new Error('targetElement is not passed in options');
+    }
 	
 }
