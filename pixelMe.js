7e7f812480edd7278cffc324b5726b15fe5a7a66
ajay <ajaysunarthi@iitj.ac.in>
2017-01-04 08:17:28 +0530

    get number of approx. tiles

--- pixelMe.js
+++ pixelMe.js
@@ -1,5 +1,5 @@
 function pixelMe(options) {
-	if (!options.image) {
+    if (!options.image) {
         throw new Error('image is not passed in options');
     }
     if (!options.targetElement) {
@@ -27,6 +27,27 @@ function pixelMe(options) {
         return destination;
     };
 
+    function process() {
+        // Number of tiles on x & y in integers
+        options.divX = Math.floor(options.width / options.tileWidth);
+        options.divY = Math.floor(options.height / options.tileHeight);
+        var context = getContext();
+        console.log(context);
+    }
+
+    function getContext() {
+        var canvas = document.createElement('canvas');
+        canvas.width = options.tileWidth * options.divX;
+        canvas.height = options.tileHeight * options.divY;
+        var context = canvas.getContext('2d');
+        context.drawImage(options.image, 0, 0, canvas.width, canvas.height);
+        return context;
+    }
+
     options = extend(defaults, options);
+
+    if (options.image.complete) {
+        process();
+    }
 
 }
